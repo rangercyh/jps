9e0c0522e264239b4e00dbcf22d4dad24abb6153
caiyiheng <rangercyh@qq.com>
2020-04-14 16:47:21 +0800

    'fix'

--- test.lua
+++ test.lua
@@ -20,6 +20,7 @@ m:add_blockset({
 for i = 0, 10 do
     m:add_block(18, i)
 end
+--[===[
 m:add_blockset({
     {1,0},{1,19},{5,3},{6,3},{6,4},{6,5},{5,5},
     {9,9},{10,9},{11,9},{12,9},{13,9},
@@ -30,24 +31,25 @@ m:add_blockset({
 m:dump()
 m:mark_connected()
 m:dump_connected()
--- m:dump()
+-- ]===]
+m:dump()
 -- m:clear_block()
 -- m:dump()
 -- m:add_block(1,1)
 -- m:dump()
--- local total_time, n = 0, 100
--- for i = 1, n do
---     local _, _, t = m:find_path()
---     total_time = total_time + t
--- end
--- -- if find then
---     m:dump()
---     print('avg time:', (total_time / n) * 1000, 'ms')
+local total_time, n = 0, 100
+for i = 1, n do
+    local _, _, t = m:find_path()
+    total_time = total_time + t
+end
+-- if find then
+    m:dump()
+    print('avg time:', (total_time / n) * 1000, 'ms')
 -- else
     -- print('no path!')
 -- end
 -- ]=]
---[[
+-- [[
 local m1 = astar.new({
     {0, 1, 1, 0, 0},
     {0, 0, 0, 0, 0},
